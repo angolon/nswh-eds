2f4f643a19f8c947bcb7da00007b6e36182ad5cd
Angus Gerry <angus.gerry@quantium.com.au>
2023-10-20 19:47:48 +1100

    Hacking together early stages of summary view

--- scenario-3/survey.sql
+++ scenario-3/survey.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE Survey (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
@@ -69,3 +71,70 @@ CREATE TABLE MultipleChoiceResponse (
     FOREIGN KEY (response) REFERENCES MultipleChoiceOption (optionId)
 );
 
+CREATE VIEW ResponseOverview AS
+SELECT
+  SR.userName,
+  SR.userEmail,
+  SR.id as surveyResponseId,
+  S.id AS surveyId,
+  S.name AS surveyName
+FROM
+  Survey AS S
+  INNER JOIN SurveyResponse AS SR
+    ON SR.surveyId = S.id
+  LEFT JOIN (
+    SELECT
+      R.surveyResponseId,
+      COUNT(Q.questionId) as NumQuestions,
+      SUM(
+        CASE R.response
+          WHEN Q.answer THEN 1
+          ELSE 0
+        END
+      ) AS NumCorrect
+    FROM
+      FreeFormResponse as R
+      INNER JOIN FreeFormQuestion AS Q
+        on R.questionId = Q.questionId
+    GROUP BY R.surveyResponseId
+  ) AS Free
+    ON SR.id = Free.surveyResponseId
+  LEFT JOIN (
+    SELECT
+      R.surveyResponseId,
+      COUNT(Q.questionId) as NumQuestions,
+      SUM(
+        CASE R.response
+          WHEN Q.answer THEN 1
+          ELSE 0
+        END
+      ) AS NumCorrect
+    FROM
+      BinaryResponse as R
+      INNER JOIN BinaryQuestion AS Q
+        on R.questionId = Q.questionId
+    GROUP BY R.surveyResponseId
+  ) AS Binary
+    ON SR.id = Binary.surveyResponseId
+  LEFT JOIN (
+    SELECT
+      R.surveyResponseId,
+      COUNT(R.response) as NumQuestions,
+      SUM(
+        CASE
+          WHEN R.Response IS Q.optionId THEN 1
+          ELSE 0
+        END
+      ) AS NumCorrect
+    FROM
+      MultipleChoiceResponse as R
+      LEFT JOIN MultipleChoiceAnswer AS Q
+        on R.response = Q.optionId
+    GROUP BY R.surveyResponseId
+  ) AS MultipleChoice
+    ON SR.id = MultipleChoice.surveyResponseId
+;
+
+-- Forces the interpreter to validate that all the joins/column names are
+-- correct and happy.
+SELECT * FROM ResponseOverview;
